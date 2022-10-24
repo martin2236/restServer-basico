086f7294f1abd041d79a9e973b74eb5f9c9fa136
martin2236 <martoxxx100@gmail.com>
2022-10-24 16:43:34 -0300

    google signIn

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,6 +2,7 @@ const {request,response} = require('express');
 const User = require('../models/usuarios');
 const {generarJWT} = require('../helpers/generarJWT')
 const bcrypt = require('bcryptjs');
+const { googleVerify } = require('../helpers/googleVerify');
 
 const login = async (req = request, res = response) => {
 
@@ -43,6 +44,44 @@ const login = async (req = request, res = response) => {
         }
 }
 
+const googleSignIn = async (req = request, res = response)=>{
+    const {id_token} = req.body;
+    try {
+        const {correo,nombre,img} = await googleVerify(id_token)
+
+        let usuario = await User.findOne({correo});
+
+        if(!usuario){
+            const data ={
+                nombre,
+                correo,
+                img,
+                rol:"USER_ROLE",
+                password:"=p",
+                google:true
+            }
+            usuario = new User(data);
+            await usuario.save();
+        }
+
+        if(!usuario.estado){
+            return res.status(401).json("usuario bloqueado")
+        }
+
+        const token = await generarJWT(usuario.id)
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            msg:"El token no se pudo verificar"
+        })
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validarCampos');
 
 const router = Router();
@@ -9,5 +9,10 @@ check('correo', 'el correo es obligatorio').isEmail(),
 check('password', 'La contraseña es obligatoria').not().isEmpty(),
 validarCampos
 ], login);
+
+router.post('/google',[
+    check('id_token', 'El id_token es necesario').not().isEmpty(),
+    validarCampos
+    ], googleSignIn);
 
 module.exports = router
